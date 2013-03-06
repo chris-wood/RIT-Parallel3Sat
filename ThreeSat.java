e7622e1740c1a4be5ed82e8ed7e962651782a75e
Eitan Romanoff <ear7631@gmail.com>
2013-03-06 12:49:14 -0500

    New features for input and testing
      - Allows pre-made configurations to be given VIA file
      - Allows a seed to be given to repeat tests with the PRNG

--- ThreeSat.java
+++ ThreeSat.java
@@ -2,60 +2,155 @@ import java.util.Random;
 import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
+
+import java.io.File;
 
 public class ThreeSat {
 
     public static void main(String args[]) {
-        int num_clauses = 0;
-        int num_literals = 0;
 
-        try {
-            num_literals = Integer.parseInt(args[0]);
-            num_clauses = Integer.parseInt(args[1]);
-        }
-        catch (Exception e) {
-            System.out.println("Usage: java ThreeSat <num_literals> <num_clauses>");
+        if (args.length < 2) {
+            System.out.println("Usage: java ThreeSat <num_literals> <num_clauses> [seed]");
             System.exit(1);
         }
 
-
-        // Generate a random problem
-        Random prng = new Random();
+        int num_clauses = 0;
+        int num_literals = 0;
 
         // This is a mapping from index to value, the literal is represented as an index
-        ArrayList<Boolean> literals = new ArrayList<Boolean>();
+        ArrayList<Boolean> literals = null;
 
         // Each clause contains three numbers (indices) to the literals structure
-        ArrayList<ArrayList<Integer>> clauses = new ArrayList<ArrayList<Integer>>();
-        ArrayList<ArrayList<Boolean>> negations = new ArrayList<ArrayList<Boolean>>();
+        ArrayList<ArrayList<Integer>> clauses = null;
+        ArrayList<ArrayList<Boolean>> negations = null;
 
-        // initialize the literals
-        for (int i = 0; i < num_literals; i++) {
-            literals.add(i, false);
-        }
+        // Read in from a file
+        if (args[0].equals("-f")) {
+            String filename = args[1];
+            try {
+                File file = new File(filename);
+                Scanner scanner = new Scanner(file);
 
-        // initialize the clauses
-        for (int i = 0; i < num_clauses; i++) {
-            ArrayList<Integer> clause = new ArrayList<Integer>();
-            ArrayList<Boolean> clause_negations = new ArrayList<Boolean>();
-            HashSet<Integer> selected = new HashSet<Integer>();
+                num_literals = Integer.parseInt(scanner.nextLine().trim());
 
-            for (int j = 0; j < 3; j++) {
-                int candidate = prng.nextInt(literals.size());
-                boolean negation = prng.nextBoolean();
+                ArrayList<String> file_lines = new ArrayList<String>();
 
-                while( selected.contains(candidate) ) {
-                    candidate = prng.nextInt(literals.size());
+                while (scanner.hasNextLine()) {
+                    num_clauses ++;
+                    file_lines.add(scanner.nextLine());
                 }
 
-                clause.add(j, candidate);
-                clause_negations.add(j, negation);
-                selected.add(candidate);
+                // Load in the literals
+                literals = new ArrayList<Boolean>();
+                for (int i = 0; i < num_literals; i++) {
+                    literals.add(i, false);
+                }
+
+                // Load in the clauses
+                clauses = new ArrayList<ArrayList<Integer>>();
+                negations = new ArrayList<ArrayList<Boolean>>();
+                for (int i = 0; i < num_clauses; i++) {
+                    ArrayList<Integer> clause = new ArrayList<Integer>();
+                    ArrayList<Boolean> clause_negations = new ArrayList<Boolean>();
+                    String[] tokens = file_lines.get(i).trim().split(" ");
+
+                    for (int j = 0; j < 3; j++) {
+                        boolean negation = true;
+                        int literal = 0;
+
+                        if (tokens[j].charAt(0) == '!') {
+                            negation = false;
+                            literal = Integer.parseInt(tokens[j].substring(1));
+                        }
+                        else {
+                            literal = Integer.parseInt(tokens[j]);
+                        }
+
+                        clause.add(j, literal);
+                        clause_negations.add(j, negation);
+                    }
+
+                    clauses.add(i, clause);
+                    negations.add(i, clause_negations);
+                }
+
+
+            }
+            catch (Exception e) {
+                System.out.println("Error: Error occured while reading file.");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        // Generate configuration
+        else {
+            try {
+                num_literals = Integer.parseInt(args[0]);
+                num_clauses = Integer.parseInt(args[1]);
+            }
+            catch (Exception e) {
+                System.out.println("Usage: java ThreeSat <num_literals> <num_clauses> [seed]");
+                System.exit(1);
+            }
+            
+
+
+            // Generate a random problem
+            Random prng = null;
+
+            if (args.length == 3) {
+                long seed = 0;
+                try {
+                    seed = Long.parseLong(args[2]);
+                }
+                catch (Exception e) {
+                    System.out.println("Error: Seed must be of long type.");
+                    System.exit(1);
+                }
+                prng = new Random(seed);
+            }
+            else {
+                prng = new Random();
             }
 
-            clauses.add(i, clause);
-            negations.add(i, clause_negations);
+            // This is a mapping from index to value, the literal is represented as an index
+            literals = new ArrayList<Boolean>();
+
+            // Each clause contains three numbers (indices) to the literals structure
+            clauses = new ArrayList<ArrayList<Integer>>();
+            negations = new ArrayList<ArrayList<Boolean>>();
+
+            // initialize the literals
+            for (int i = 0; i < num_literals; i++) {
+                literals.add(i, false);
+            }
+
+            // initialize the clauses
+            for (int i = 0; i < num_clauses; i++) {
+                ArrayList<Integer> clause = new ArrayList<Integer>();
+                ArrayList<Boolean> clause_negations = new ArrayList<Boolean>();
+                HashSet<Integer> selected = new HashSet<Integer>();
+
+                for (int j = 0; j < 3; j++) {
+                    int candidate = prng.nextInt(literals.size());
+                    boolean negation = prng.nextBoolean();
+
+                    while( selected.contains(candidate) ) {
+                        candidate = prng.nextInt(literals.size());
+                    }
+
+                    clause.add(j, candidate);
+                    clause_negations.add(j, negation);
+                    selected.add(candidate);
+                }
+
+                clauses.add(i, clause);
+                negations.add(i, clause_negations);
+            }
         }
+
 
         // Print the clause prettily
         System.out.println("Looking for a solution to the following problem:");
@@ -72,7 +167,7 @@ public class ThreeSat {
                     System.out.print("!");
                 }
 
-                System.out.print(clause.get(j));
+                System.out.print("" + clause.get(j));
 
                 if (j != 2) {
                     System.out.print(" || ");
