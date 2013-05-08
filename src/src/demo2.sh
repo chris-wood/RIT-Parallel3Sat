979c949c99cd651170a1272721980b3e6d250c4f
Ankur Bajoria <arb2572@rit.edu>
2013-05-08 00:51:42 -0400

    Just fixed a space

--- src/src/demo2.sh
+++ src/src/demo2.sh
@@ -4,12 +4,12 @@ echo "-----------------------------"
 read -p "Press key to run..."
 echo "-----------------------------"
 echo "java  -Xmx2000m ThreeSatExhaustiveSeq 26 100 12345"
-java  -Xmx2000m ThreeSatExhaustiveSeq 26 100 12345
+java -Xmx2000m ThreeSatExhaustiveSeq 26 100 12345
 echo "-----------------------------"
 read -p "Press key to run parallel version with guided schedule..."
 echo "-----------------------------"
 echo "java  -Xmx2000m -Dpj.nt=2 ThreeSatExhaustiveSmp 26 100 12345"
-java  -Xmx2000m -Dpj.nt=2 -Dpj.schedule="guided" ThreeSatExhaustiveSmp 26 100 12345
+java -Xmx2000m -Dpj.nt=2 -Dpj.schedule="guided" ThreeSatExhaustiveSmp 26 100 12345
 echo ""
 echo "Do they match? :-)"
 echo "What's the speedup?"
